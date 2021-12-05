68dfd41bbe51c3535c1fd3bebbd02bff5576a572
sfj.eth <0x0xsfj@gmail.com>
2021-12-04 23:32:01 -0800

    Rebase Timer Component

---
+++ components/Staking/Rebase.tsx
@@ -0,0 +1,9 @@
+import { Text } from '@chakra-ui/react';
+
+const Rebase = () => {
+  const rebaseTimeSeconds = 1200000;
+
+  return <Text>Rebase in {rebaseTimeSeconds}</Text>;
+};
+
+export default Rebase;
