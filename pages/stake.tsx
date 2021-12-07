2d2f15487a8f274fc82f395219afcc18e284e227
sfj.eth <0x0xsfj@gmail.com>
2021-12-06 23:51:27 -0800

    Convert to Stake Buttons

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import {
   Box,
@@ -18,12 +19,16 @@ import {
   Container,
   Divider,
   useColorModeValue,
+  Input,
 } from '@chakra-ui/react';
 import { BiWalletAlt } from 'react-icons/bi';
 import Layout from '../components/Layout';
 import PoolTable from '../components/Staking/PoolTable';
 
 const Stake = () => {
+  const [isStaking, setIsStaking] = useState(false);
+  const [amount, setAmount] = useState(null);
+
   return (
     <>
       <Head>
@@ -63,94 +68,85 @@ const Stake = () => {
               </Text>
             </Box>
           </SimpleGrid>
-          <Flex justify="center" mt="10">
-            <Tabs w="100%">
-              <TabList justifyContent="center">
-                <Tab
-                  fontWeight="bold"
-                  textTransform="uppercase"
-                  _selected={{ color: 'brand.primary' }}
-                >
-                  Stake
-                </Tab>
-                <Tab
-                  fontWeight="bold"
-                  textTransform="uppercase"
-                  _selected={{ color: 'brand.primary' }}
-                >
-                  Unstake
-                </Tab>
-              </TabList>
+          <VStack justify="center" mt="10">
+            <HStack justifyContent="center" spacing={10} mb="8">
+              <Button onClick={() => setIsStaking(true)}>Stake</Button>
+              <Button>Unstake</Button>
+            </HStack>
+            <HStack justifyContent="center" spacing={4} maxW="600px" pb="8">
+              <Input placeholder="Amount" value={amount} borderRadius="full" />
+              <Button colorScheme="green">Stake</Button>
+            </HStack>
 
-              <TabPanels w="100%">
-                <TabPanel>
-                  <HStack spacing={10} justifyContent="center" my="12">
-                    <VStack>
-                      <Text>
-                        First time staking <b>GAIA</b>?
-                      </Text>
-                      <Text>Please approve GAIA DAO to stake your $GAIA.</Text>
-                    </VStack>
-                    <Button variantColor="blue" variant="outline" size="lg">
-                      Approve
-                    </Button>
-                  </HStack>
-                  <Container>
-                    <HStack justifyContent="space-between" mb="4">
-                      <Text fontSize="xl">Unstaked Balance</Text>
-                      <Text fontSize="xl" fontWeight="bold">
-                        0 GAIA
-                      </Text>
-                    </HStack>
-                    <HStack justifyContent="space-between" mb="4">
-                      <Text fontSize="xl">Staked Balance</Text>
-                      <Text fontSize="xl" fontWeight="bold">
-                        0 sGAIA
-                      </Text>
-                    </HStack>
-                    <HStack justifyContent="space-between" mb="4">
-                      <Text opacity="0.6" ml="4">
-                        Single Staking
-                      </Text>
-                      <Text opacity="0.6" fontWeight="bold">
-                        0 GAIA
-                      </Text>
-                    </HStack>
-                    <HStack justifyContent="space-between" mb="4">
-                      <Text opacity="0.6" ml="4">
-                        Wrapped Balance
-                      </Text>
-                      <Text opacity="0.6" fontWeight="bold">
-                        0 wsGAIA
-                      </Text>
-                    </HStack>
-                    <Divider mb="4" />
-                    <HStack justifyContent="space-between" mb="4">
-                      <Text fontSize="xl">Next Reward Amount</Text>
-                      <Text fontSize="xl" fontWeight="bold">
-                        0 sGAIA
-                      </Text>
-                    </HStack>
-                    <HStack justifyContent="space-between" mb="4">
-                      <Text fontSize="xl">Next Reward Yield</Text>
-                      <Text fontSize="xl" fontWeight="bold">
-                        0.53432%
-                      </Text>
-                    </HStack>
-                    <HStack justifyContent="space-between" mb="4">
-                      <Text fontSize="xl">ROI(5-Day Rate)</Text>
-                      <Text fontSize="xl" fontWeight="bold">
-                        9.0432%
-                      </Text>
-                    </HStack>
-                  </Container>
-                </TabPanel>
-                <TabPanel>
-                  <p>two!</p>
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
-          </Flex>
+            <VStack spacing="8" my="12">
+              <HStack spacing={10} justifyContent="center">
+                <VStack>
+                  <Text>
+                    First time staking <b>GAIA</b>?
+                  </Text>
+                  <Text>Please approve GAIA DAO to stake your $GAIA.</Text>
+                </VStack>
+                <Button variantColor="blue" variant="outline" size="lg">
+                  Approve
+                </Button>
+              </HStack>
+              <Text textAlign="center" fontSize="sm">
+                {`Note: The "Approve" transaction is only needed when
+                        staking/unstaking for the first time; subsequent
+                        staking/unstaking only requires you to perform the
+                        "Stake" or "Unstake" transaction.`}
+              </Text>
+            </VStack>
+            <Container>
+              <HStack justifyContent="space-between" mb="4">
+                <Text fontSize="xl">Unstaked Balance</Text>
+                <Text fontSize="xl" fontWeight="bold">
+                  0 GAIA
+                </Text>
+              </HStack>
+              <HStack justifyContent="space-between" mb="4">
+                <Text fontSize="xl">Staked Balance</Text>
+                <Text fontSize="xl" fontWeight="bold">
+                  0 sGAIA
+                </Text>
+              </HStack>
+              <HStack justifyContent="space-between" mb="4">
+                <Text opacity="0.6" ml="4">
+                  Single Staking
+                </Text>
+                <Text opacity="0.6" fontWeight="bold">
+                  0 GAIA
+                </Text>
+              </HStack>
+              <HStack justifyContent="space-between" mb="4">
+                <Text opacity="0.6" ml="4">
+                  Wrapped Balance
+                </Text>
+                <Text opacity="0.6" fontWeight="bold">
+                  0 wsGAIA
+                </Text>
+              </HStack>
+              <Divider mb="4" />
+              <HStack justifyContent="space-between" mb="4">
+                <Text fontSize="xl">Next Reward Amount</Text>
+                <Text fontSize="xl" fontWeight="bold">
+                  0 sGAIA
+                </Text>
+              </HStack>
+              <HStack justifyContent="space-between" mb="4">
+                <Text fontSize="xl">Next Reward Yield</Text>
+                <Text fontSize="xl" fontWeight="bold">
+                  0.53432%
+                </Text>
+              </HStack>
+              <HStack justifyContent="space-between" mb="4">
+                <Text fontSize="xl">ROI (5-Day Rate)</Text>
+                <Text fontSize="xl" fontWeight="bold">
+                  9.0432%
+                </Text>
+              </HStack>
+            </Container>
+          </VStack>
         </Box>
         <Box
           border="1px"
